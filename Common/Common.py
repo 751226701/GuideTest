67a534ac00081272a39dafaa2cee7cfe8fc6bd56
刘涛 <751226701@qq.com>
2025-07-28 15:10:42 +0800

    优化

--- Common/Common.py
+++ Common/Common.py
@@ -194,9 +194,25 @@ class Common:
             self._log_operation(operation or f"输入文件{file_path}", "FAILED", duration, str(e))
             raise
 
-    
+    @staticmethod
+    def auto_image_map(root_dir=Config.template_dir):
+        image_map = {}
+        for dirpath, _, filenames in os.walk(root_dir):
+            for filename in filenames:
+                if filename.endswith('.png'):
+                    name_no_ext = os.path.splitext(filename)[0]
+                    rel_path = os.path.join(dirpath, filename).replace("\\", "/")
+                    image_map[name_no_ext] = rel_path
+        return image_map
 
-
+    @staticmethod
+    def auto_file_map(root_dir=Config.test_files_dir):
+        file_map = {}
+        for dirpath, _, filenames in os.walk(root_dir):
+            for filename in filenames:
+                rel_path = os.path.join(dirpath, filename)
+                file_map[filename] = rel_path
+        return file_map
 
     @staticmethod
     def run_steps(steps, eng, com):
--- Pages/Engineer.py
+++ Pages/Engineer.py
@@ -5,38 +5,15 @@
 # @file: Engineer.py
 # @project: EAI_2.0_GUI_TEST
 import allure
-import time
-import os
-from Config.Config import Config
-from retry import retry
 from Common.Common import Common
 
-def auto_image_map(root_dir=Config.template_dir):
-    image_map = {}
-    for dirpath, _, filenames in os.walk(root_dir):
-        for filename in filenames:
-            if filename.endswith('.png'):
-                name_no_ext = os.path.splitext(filename)[0]
-                rel_path = os.path.join(dirpath, filename).replace("\\", "/")
-                image_map[name_no_ext] = rel_path
-    return image_map
-
-def auto_file_map(root_dir=Config.test_files_dir):
-    file_map = {}
-    if os.path.exists(root_dir):
-        for dirpath, _, filenames in os.walk(root_dir):
-            for filename in filenames:
-                rel_path = os.path.join(dirpath, filename)
-                file_map[filename] = rel_path
-    return file_map
 
 class Engineer:
 
-    IMAGE_MAP = auto_image_map()
-    FILE_MAP = auto_file_map()
-
     def __init__(self, common_instance=None):
         self.common = common_instance if common_instance else Common()
+        self.IMAGE_MAP = self.common.auto_image_map()
+        self.FILE_MAP = self.common.auto_file_map()
 
     @allure.step("{operation}")
     def click(self, image_key, operation, timeout=10, **kwargs):
@@ -75,8 +52,4 @@ class Engineer:
         file_path = self.FILE_MAP[file_key]
         if not file_path:
             raise ValueError(f"未找到文件路径: {file_key}")
-        self.common.input_file(file_path, operation or f"输入文件{file_key}", **kwargs)
-
-if __name__ == "__main__":
-    # print(auto_image_map())
-    print(auto_file_map())
+        self.common.input_file(file_path, operation or f"输入文件{file_key}")
